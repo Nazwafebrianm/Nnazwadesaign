d2ed850f58df6374da2bcf75e57f005e7d7d7d48
nazwafebrian80-hue <nazwa.febrian80@sma.belajar.id>
2026-08-06 10:21:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function filterItems(category) {
+  let items = document.querySelectorAll(".card");
+
+  items.forEach(item => {
+    if (category === "all") {
+      item.style.display = "block";
+    } else {
+      if (item.classList.contains(category)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    }
+  });
+}
